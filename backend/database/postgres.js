ac2582b62675597bde44c379d27ef03bf327b364
Pedro Carvalho <pedro.carvalho@dmslog.com>
2023-09-26 16:16:58 -0300

    add .env.example

--- backend/database/postgres.js
+++ backend/database/postgres.js
@@ -6,7 +6,7 @@ dotenv.config()
 const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
   host: 'postgres',
   dialect: 'postgres',
-  port: '5432'
+  port: '5433'
 });
 
 export default sequelize
